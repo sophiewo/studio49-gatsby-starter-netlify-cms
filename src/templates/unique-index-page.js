9391b056fec4b45acec1ae081ebe20a29a50b950
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2021-06-30 14:04:41 +0100

    creates unique index page for new collection

---
+++ src/templates/unique-index-page.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+
+export const UniqueIndexPageTemplate = ({
+  image,
+  title,
+}) => (
+  <div>
+    <div
+      className="full-width-image margin-top-0"
+      style={{
+        backgroundImage: `url(${!!image.childImageSharp ? image.childImageSharp.fluid.src : image
+          })`,
+        backgroundPosition: `top left`,
+        backgroundAttachment: `fixed`,
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          height: '150px',
+          lineHeight: '1',
+          justifyContent: 'space-around',
+          alignItems: 'left',
+          flexDirection: 'column',
+        }}
+      >
+        <h1
+          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+          style={{
+            boxShadow:
+              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(255, 68, 0)',
+            color: 'white',
+            lineHeight: '1',
+            padding: '0.25em',
+          }}
+        >
+          {title}
+        </h1>
+      </div>
+    </div>
+  </div>
+)
+
+UniqueIndexPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+}
+
+const UniqueIndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <UniqueIndexPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+      />
+    </Layout>
+  )
+}
+
+UniqueIndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default UniqueIndexPage
+
+export const pageQuery = graphql`
+  query UniqueIndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "unique-index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+       }
+      }
+    }
+`
